28bd2607624acd091afe78570eede4eade9b47a4
Benyamin Jafari <benyawminjawfari@gmail.com>
2023-07-10 14:51:38 +0200

    data model done

--- test/features/number_trivia/data/models/number_trivia_model_test.dart
+++ test/features/number_trivia/data/models/number_trivia_model_test.dart
@@ -23,5 +23,27 @@ void main() {
       // assert
       expect(result, tNumberTriviaModel);
     });
+
+    test(
+        'should return a valid number when the JSON number is regarded as a double',
+        () async {
+      // arrange
+      final Map<String, dynamic> jsonMap =
+          json.decode(fixture('trivia_double.json'));
+      // act
+      final result = NumberTriviaModel.fromJson(jsonMap);
+      // assert
+      expect(result, tNumberTriviaModel);
+    });
+  });
+
+  group('toJson', () {
+    test('should return a JSON map containing the proper data', () {
+      // act
+      final result = tNumberTriviaModel.toJson();
+      // assert
+      final expectedMap = {"text": "Test Text", "number": 1};
+      expect(result, expectedMap);
+    });
   });
 }
